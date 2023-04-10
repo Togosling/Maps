6c1fa51ea2c32a78b34473e408826ffd1620cf0d
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-10 10:14:01 +0800

    first commit

---
+++ iOS_Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Maps
+//
+//  Created by Тагай Абдылдаев on 10/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
