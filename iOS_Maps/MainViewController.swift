5093dc09c3e479171ddf17b65ff9919a168fc30a
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-12 11:48:36 +0800

    MapSearch

--- iOS_Maps/MainViewController.swift
+++ iOS_Maps/MainViewController.swift
@@ -17,6 +17,31 @@ extension MainViewController: MKMapViewDelegate {
         return annotationView
     }
 }
+extension MKMapItem {
+    func address() -> String {
+        var address = ""
+        if placemark.subThoroughfare != nil {
+            address = placemark.subThoroughfare! + " "
+        }
+        if placemark.thoroughfare != nil {
+            address = placemark.thoroughfare! + " "
+        }
+        if placemark.postalCode != nil {
+            address = placemark.postalCode! + " "
+        }
+        if placemark.locality != nil {
+            address = placemark.locality! + " "
+        }
+        if placemark.administrativeArea != nil {
+            address = placemark.administrativeArea! + " "
+        }
+        if placemark.country != nil {
+            address = placemark.country! + " "
+        }
+        return address
+    }
+}
+
 
 class MainViewController: UIViewController {
     
@@ -29,14 +54,42 @@ class MainViewController: UIViewController {
         
         setupViews()
         setNewRegion()
-        setAnnotationsForMap()
-        performLocalSerch()
+        setSearchUI()
+        
+                
+    }
+    let searchTextField = UITextField()
+
+    
+    fileprivate func setSearchUI() {
+        
+        searchTextField.placeholder = "Search"
+        
+        let whiteConttainer = UIView()
+        whiteConttainer.backgroundColor = .white
+        view.addSubview(whiteConttainer)
+        whiteConttainer.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(50)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        whiteConttainer.addSubview(searchTextField)
+        searchTextField.snp.makeConstraints { make in
+            make.size.equalToSuperview()
+            make.leading.equalToSuperview().offset(16)
+        }
+        searchTextField.addTarget(self, action: #selector(handleSearch), for: .editingChanged)
         
     }
     
+    @objc func handleSearch() {
+        performLocalSerch()
+    }
+    
     fileprivate func performLocalSerch() {
+
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = "Apple"
+        request.naturalLanguageQuery = searchTextField.text
         request.region = mapView.region
         let localSearch = MKLocalSearch(request: request)
         localSearch.start { resp, err in
@@ -44,30 +97,9 @@ class MainViewController: UIViewController {
                 print("Failed", err)
                 return
             }
+            self.mapView.removeAnnotations(self.mapView.annotations)
             resp?.mapItems.forEach({ mapItem in
-                
-                let placeMark = mapItem.placemark
-                var address = ""
-                if placeMark.subThoroughfare != nil {
-                    address = placeMark.subThoroughfare! + " "
-                }
-                if placeMark.thoroughfare != nil {
-                    address = placeMark.thoroughfare! + " "
-                }
-                if placeMark.postalCode != nil {
-                    address = placeMark.postalCode! + " "
-                }
-                if placeMark.locality != nil {
-                    address = placeMark.locality! + " "
-                }
-                if placeMark.administrativeArea != nil {
-                    address = placeMark.administrativeArea! + " "
-                }
-                if placeMark.country != nil {
-                    address = placeMark.country! + " "
-                }
-                print(address)
-                
+
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = mapItem.placemark.coordinate
                 annotation.title = mapItem.name
@@ -78,28 +110,28 @@ class MainViewController: UIViewController {
         }
     }
     
-    fileprivate func setAnnotationsForMap() {
-        let PekingUniAnnotation = MKPointAnnotation()
-        PekingUniAnnotation.coordinate = CLLocationCoordinate2D(latitude: 39.992040, longitude: 116.315428)
-        PekingUniAnnotation.title = "Peking University"
-        PekingUniAnnotation.subtitle = "Haidian"
-        mapView.addAnnotation(PekingUniAnnotation)
-        
-        let QinghuaUniAnnotation = MKPointAnnotation()
-        QinghuaUniAnnotation.coordinate = CLLocationCoordinate2D(latitude: 39.99929, longitude: 116.32705)
-        QinghuaUniAnnotation.title = "Qinghua University"
-        QinghuaUniAnnotation.subtitle = "Haidian"
-        mapView.addAnnotation(QinghuaUniAnnotation)
-        
-        mapView.showAnnotations(self.mapView.annotations, animated: true)
-        
-    }
     
     fileprivate func setNewRegion() {
-        let centerCoordinate = CLLocationCoordinate2D(latitude: 39.992040, longitude: 116.315428)
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 39.924583, longitude: 116.407753)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: centerCoordinate, span: span)
         mapView.setRegion(region, animated: true)
+    }
+    
+    fileprivate func setAnnotationsForMap() {
+        let Alcatras = MKPointAnnotation()
+        Alcatras.coordinate = CLLocationCoordinate2D(latitude: 37.826471, longitude:  -122.421979)
+        Alcatras.title = "Alcatras"
+        Alcatras.subtitle = "CA"
+        mapView.addAnnotation(Alcatras)
+
+        let SienceAcademy = MKPointAnnotation()
+        SienceAcademy.coordinate = CLLocationCoordinate2D(latitude: 37.771401, longitude: -122.466439)
+        SienceAcademy.title = "Sience Academy"
+        SienceAcademy.subtitle = "CA"
+        mapView.addAnnotation(SienceAcademy)
+
+        mapView.showAnnotations(self.mapView.annotations, animated: true)
     }
     
     fileprivate func setupViews() {
@@ -109,7 +141,6 @@ class MainViewController: UIViewController {
             make.size.equalToSuperview()
         }
     }
-
-
+    
 }
 
